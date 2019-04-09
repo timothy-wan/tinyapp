ac013a0b9c9069cbc1794e538a7eed3e8e6dd166
tw5033 <tw5033@gmail.com>
2019-04-09 23:24:19 +0000

    added delete buttons for urls

--- express_server.js
+++ express_server.js
@@ -36,6 +36,11 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${newShortURL}`);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+})
+
 app.get('/urls/new', (req, res) => {
   res.render("urls_new");
 });
